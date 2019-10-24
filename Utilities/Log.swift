31842910c7445eb00f255db1afd0465ae0246190
ekkotech <andrew.coad@ekko-tech.com>
2019-10-24 15:21:12 +0900

    Updated Log.swift

--- Utilities/Log.swift
+++ Utilities/Log.swift
@@ -1,8 +1,8 @@
 //
 //  Log.swift
-//  IOTCourse4
+//  IOTCourse3
 //
-//  Created by Andrew Coad on 22/10/2019.
+//  Created by Andrew Coad on 24/10/2019.
 //  Copyright © 2019 Andrew Coad. All rights reserved.
 //
 
@@ -11,6 +11,11 @@ import os
 
 private let subsystem = Bundle.main.bundleIdentifier ?? "Unamed"
 
+/**
+ Subsystem and categories are provided to facilitate seaching and filtering the log content.  Susbsytem will always be the bundle identifier; categories can be added as required
+ */
 struct Log {
-  static let ble = OSLog(subsystem: subsystem, category: "bluetooth")
+    static let ble = OSLog(subsystem: subsystem, category: "bluetooth")
+    static let model = OSLog(subsystem: subsystem, category: "model")
+    static let ui = OSLog(subsystem: subsystem, category: "ui")
 }
